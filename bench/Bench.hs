38c553ceef52662880cb57cfcc803f0bd0a7dc9a
Patrick Steele <steele.pat@gmail.com>
2022-12-27 17:34:13 -0500

    Benchmark the maximum sub array problem

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
+import Algorithms.MaximumSubArray
 import Algorithms.Sort.InsertionSort
+import Control.DeepSeq
 import Control.Monad
 import Criterion.Main
 import qualified Data.Vector as V
@@ -8,10 +11,10 @@ import System.Random
 import System.Random.Stateful
 
 randomVec :: StatefulGen g IO => Int -> g -> IO (V.Vector Double)
-randomVec n = fmap V.fromList . replicateM n . uniformRM (-1e100, 1e100)
+randomVec n = fmap V.fromList . replicateM n . uniformRM (-1e10, 1e10)
 
 genSizes :: Int -> [Int]
-genSizes = fmap (10 ^) . enumFromTo 0
+genSizes = fmap (2 ^) . enumFromTo 0
 
 orderedTestCases :: [Int] -> [V.Vector Double]
 orderedTestCases sizes = [V.fromList (fmap fromIntegral [1 .. size]) | size <- sizes]
@@ -26,21 +29,49 @@ randomTestCases seed sizes = do
   forM sizes $ \size -> do
     randomVec size g
 
-main :: IO ()
-main =
-  let insertionSortSizes = genSizes 3
+mkInsertionSortBenchmarks :: IO [Benchmark]
+mkInsertionSortBenchmarks =
+  let insertionSortSizes = genSizes 10
 
-      mkInsertionSortBench v = bench (show (V.length v)) (nf insertionSort v)
+      mkBench v = bench (show (V.length v)) (nf insertionSort v)
    in do
         randomCases <- randomTestCases 0 insertionSortSizes
-        defaultMain
+        pure
           [ bgroup
               "insertionSort (ordered)"
-              (fmap mkInsertionSortBench (orderedTestCases insertionSortSizes)),
+              (fmap mkBench (orderedTestCases insertionSortSizes)),
             bgroup
               "insertionSort (reversed)"
-              (fmap mkInsertionSortBench (reversedTestCases insertionSortSizes)),
+              (fmap mkBench (reversedTestCases insertionSortSizes)),
             bgroup
               "insertionSort (random)"
-              (fmap mkInsertionSortBench randomCases)
+              (fmap mkBench randomCases)
           ]
+
+instance NFData Slice where
+  rnf (Slice i j) = seq i (seq j ())
+
+instance NFData a => NFData (SubArraySum a) where
+  rnf (SubArraySum x y) = seq x (seq y ())
+
+mkMaximumSubArrayBenchmarks :: IO [Benchmark]
+mkMaximumSubArrayBenchmarks =
+  let mkBench v = bench (show (V.length v)) (nf maximumSubArrayNum v)
+   in do
+        randomCases <- randomTestCases 0 (genSizes 20)
+
+        pure
+          [ bgroup
+              "maximumSubArray (random)"
+              (fmap mkBench randomCases)
+          ]
+
+main :: IO ()
+main =
+  let groups =
+        [ mkInsertionSortBenchmarks,
+          mkMaximumSubArrayBenchmarks
+        ]
+   in do
+        benchmarks <- fmap concat (sequence groups)
+        defaultMain benchmarks
