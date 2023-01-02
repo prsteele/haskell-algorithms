2adff7a3badb1f7b1d1e56e7913068fd3b89be5d
Patrick Steele <steele.pat@gmail.com>
2023-01-01 22:54:35 -0500

    Support mutable sort-on algorithms

--- src/Algorithms/Sorting/Utility.hs
+++ src/Algorithms/Sorting/Utility.hs
@@ -5,13 +5,15 @@
 
 module Algorithms.Sorting.Utility where
 
+import Control.Monad.Primitive
 import Data.Ord (comparing)
 import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Generic.Mutable as MG
 
 -- | Transform a sort-by function into a sort-on function.
 mkSortOn ::
   (G.Vector v a, G.Vector v (b, a), Ord b) =>
-  -- | The sort-by function
+  -- | The sort-by sorting algorithm
   (((b, a) -> (b, a) -> Ordering) -> v (b, a) -> v (b, a)) ->
   -- | The sort-on function
   ((a -> b) -> v a -> v a)
@@ -19,3 +21,18 @@ mkSortOn ::
 mkSortOn sortBy = \f v ->
   let tag x = let y = f x in y `seq` (y, x)
    in G.map snd (sortBy (comparing fst) (G.map tag v))
+
+-- | Transform a sort-by function into a sort-on function.
+mkMutSortOn ::
+  (PrimMonad m, G.Vector v a, G.Vector v (b, a), Ord b) =>
+  -- | The sort-by sorting algorithm
+  (((b, a) -> (b, a) -> Ordering) -> G.Mutable v (PrimState m) (b, a) -> m ()) ->
+  -- | The sort-on function
+  ((a -> b) -> G.Mutable v (PrimState m) a -> m ())
+{-# INLINEABLE mkMutSortOn #-}
+mkMutSortOn mutSortBy = \f mv ->
+  let tag x = let y = f x in y `seq` (y, x)
+   in do
+        tagged <- G.freeze mv >>= G.thaw . G.map tag
+        mutSortBy (comparing fst) tagged
+        MG.iforM_ mv $ \i _ -> MG.read tagged i >>= MG.write mv i . snd
