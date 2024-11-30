2aa000b65d30caf9e57e3aacaf5a9a5da31c8a01
Patrick Steele <prsteele@proton.me>
2024-11-30 11:11:49 -0500

    chore: update to nixos-24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Common data structures and algorithms";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     systems.url = "github:nix-systems/default";
   };
 
