7fd7d378bffcb954b8449d9fbcd8286ae0dc4f1e
Patrick Steele <prsteele@proton.me>
2025-01-07 21:09:47 -0500

    test(hash tables): with a bad hash function

--- test/Data/HashTable/OpenSpec.hs
+++ test/Data/HashTable/OpenSpec.hs
@@ -13,10 +13,20 @@ spec :: Spec
 spec = do
   describe "Insertion" $ do
     prop "Just v == insert k v >> lookup k" insertLookupProp
+  describe "Insertion (with a bad hash)" $ do
+    prop "Just v == insert k v >> lookup k" insertLookupBadHashProp
 
 insertLookupProp :: [(Int, Int)] -> Property
 insertLookupProp kvs = monadicIO $ do
   t <- run (H.new hash H.linear 0)
+  forM_ kvs $ \(k, v) -> do
+    run $ H.insert t k v
+    mv <- run (H.lookup t k)
+    mv ==? Just v
+
+insertLookupBadHashProp :: [(Int, Int)] -> Property
+insertLookupBadHashProp kvs = monadicIO $ do
+  t <- run (H.new (const 0) H.linear 0)
   forM_ kvs $ \(k, v) -> do
     run $ H.insert t k v
     mv <- run (H.lookup t k)
